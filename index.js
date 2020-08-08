f7b46d98f58b02b9621d2497412bd4947228bae2
bdbose <57151177+bdbose@users.noreply.github.com>
2020-08-08 23:48:50 +0530

    Edit button

--- index.js
+++ index.js
@@ -9,16 +9,47 @@ const item=()=>{
         var list_data=document.createElement('div')
         list_data.className='list-data'
         list_data.id='list-id'
-        list_data.innerText=input_value
+        let data=document.createElement('span')
+        data.id='span-data'
+        data.innerText=input_value
         list.appendChild(list_data)
-
+        list_data.appendChild(data)
+        
         let removebtn=document.createElement('button')
         removebtn.id='del-btn'
         removebtn.innerText="Delete"
         
-        list_data.appendChild(removebtn)
-        input.value=""
+        let editbtn=document.createElement('button')
+        editbtn.id='edit-btn'
+        editbtn.innerText='Edit'
         
+        list_data.appendChild(editbtn)
+        list_data.appendChild(removebtn)
+        
+        input.value=""
+
+        let inputed = document.createElement('input')
+        inputed.id='input-edit'
+        
+        editbtn.addEventListener('click',(e)=>{
+            if(editbtn.innerText=='Edit'){
+                let v=data.innerText
+                inputed.value=v
+                editbtn.innerText='Save'
+                list_data.replaceChild(inputed,data)
+            }
+            else{
+                if(inputed.value.trim()!="")
+                    {
+                        data.innerText=inputed.value
+                        editbtn.innerText='Edit'
+                        list_data.replaceChild(data,inputed)        
+                    }
+                else{
+                    alert('Cant Be Empty')
+                }
+            }})
+
         removebtn.addEventListener('click',(e)=>{
             list.removeChild(removebtn.parentNode)  
         })
